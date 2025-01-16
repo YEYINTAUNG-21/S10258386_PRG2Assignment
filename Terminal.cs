95c4a877eacf3d6689dc7da82dd1bc6ab5c01379
YEYINTAUNG-21 <S10268975@connect.np.edu.sg>
2025-01-16 15:23:17 +0800

    Add Class headers

---
+++ Terminal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10268975C_PRG2Assignment
+{
+    public class Terminal
+    {
+        public string TerminalName { get; set; }
+        public Dictionary<string, Airline> Airlines { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+        public Dictionary<string, double> GateFees { get; set; }
+
+
+    }
+}
